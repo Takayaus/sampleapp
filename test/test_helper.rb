1c93a2ec43f409c98b6a4e100d7c6f083d69e6af
takayamori <morit617@gmail.com>
2020-05-04 16:07:05 +0900

    Fin10

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,8 +9,24 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+
+  def is_logged_in?
+    !session[:user_id].nil? #ヘルパーメソッドからテストに呼び出せないため
+  end
+
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
 end
 
-def is_logged_in?
-  !session[:user_id].nil? #ヘルパーメソッドからテストに呼び出せないため
+class ActionDispatch::IntegrationTest
+
+  # テストユーザーとしてログインする
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
+  
 end
